23bcb7c4420538a18a83852c6e412395654d5757
shervin <shervinrajabi2016@gmail.com>
2025-04-27 17:35:32 +0330

    add three methods for open, save and new file.

--- src/editor/FileManager.java
+++ src/editor/FileManager.java
@@ -1,29 +1,49 @@
 package editor;
 
 import javax.swing.*;
+import java.io.*;
 
 public class FileManager {
+
     public static void openFile(TextEditor textEditor, JTextArea textArea) {
         JFileChooser fileChooser = new JFileChooser();
         if (fileChooser.showOpenDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-            //File file = fileChooser.getSelectedFile();
-            //TODO
+            File file = fileChooser.getSelectedFile();
+            textEditor.currentFile = file;
+            try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                textArea.read(reader, null);
+            }catch (IOException e){
+                e.printStackTrace();
+
+            }
         }
     }
 
-    public static void saveFile(TextEditor textEditor, JTextArea textArea) {
+    public static void saveFile(TextEditor textEditor, JTextArea textArea){
         if (textEditor.currentFile == null){
             JFileChooser fileChooser = new JFileChooser();
             if (fileChooser.showSaveDialog(textEditor) == JFileChooser.APPROVE_OPTION) {
-                //TODO
+                File file = fileChooser.getSelectedFile();
+                textEditor.currentFile = file;
+                try(BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+                    textArea.write(writer);
+
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             }
         }
         else{
-            //TODO
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(textEditor.currentFile))) {
+                textArea.write(writer);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
         }
     }
 
     public static void newFile(TextEditor textEditor, JTextArea textArea) {
-        //TODO
+        textArea.setText("");
+        textEditor.currentFile = null;
     }
 }
